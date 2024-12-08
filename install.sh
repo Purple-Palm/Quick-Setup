f8ce011ae039daa3d18eb22c09c386bde4aefe4b
Cactus <88003472+Purple-Palm@users.noreply.github.com>
2024-12-07 23:02:46 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,44 +1,38 @@
 #!/bin/sh
-# Update and install necessary dependencies
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
-
-# Download and install OpenSSL
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-
-# Create miner directory and navigate into it
-mkdir -p ~/ccminer
+mkdir ~/ccminer
 cd ~/ccminer
 
-# Fetch the latest release from GitHub
+# Download the latest release
 GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/Android-Mining/releases?per_page=1" | jq -c '[.[] | del (.body)]')
 GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .browser_download_url")
 GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .name")
 
 echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
-
 wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer/ccminer
 wget https://raw.githubusercontent.com/Purple-Palm/Quick-Setup/main/config.json -O ~/ccminer/config.json
 chmod +x ~/ccminer/ccminer
 
-# Ask for a custom username
-echo "Enter username for this miner (e.g., hmd_01): "
-read CUSTOM_USERNAME
+# Prompt for the miner username
+read -p "Enter username for this miner: " MINER_USERNAME
 
-# Update the config.json with the custom username
-sed -i "s/UNNAMED_MINER/${CUSTOM_USERNAME}/g" ~/ccminer/config.json
+# Update the configuration file with the provided username
+sed -i "s/UNNAMED_MINER/$MINER_USERNAME/" ~/ccminer/config.json
 
-# Create a start script
+# Create the start script
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
 ~/ccminer/ccminer -c ~/ccminer/config.json
 EOF
 chmod +x start.sh
 
-# Final instructions
+# Completion message
 echo "Setup nearly complete."
-echo "Edit the config with \"nano ~/ccminer/config.json\" if necessary."
-echo "Start the miner with \"cd ~/ccminer; ./start.sh\"."
+echo "Edit the config with \"nano ~/ccminer/config.json\" if needed."
+
+echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
